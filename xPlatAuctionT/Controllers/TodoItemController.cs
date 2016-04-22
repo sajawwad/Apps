4c6be536ed40e52f38e141f0f35e73957339209a
sajawwad <sajawwad@hotmail.com>
2016-04-22 14:49:34 +0800

    First change to file

--- xPlatAuctionT/Controllers/TodoItemController.cs
+++ xPlatAuctionT/Controllers/TodoItemController.cs
@@ -36,6 +36,7 @@ namespace xPlatAuctionT.Controllers
         // PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
         public Task<TodoItem> PatchTodoItem(string id, Delta<TodoItem> patch)
         {
+            //Change test for git
             return UpdateAsync(id, patch);
         }
 
