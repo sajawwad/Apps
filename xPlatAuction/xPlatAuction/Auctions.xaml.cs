16ce0178ceafebe2b7741f08cb5c800471de14a4
Ahmed Jawad <sajawwad@hotmail.com>
2016-04-22 19:07:24 +0800

    App xaml and other files

---
+++ xPlatAuction/xPlatAuction/Auctions.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace xPlatAuction
+{
+	public partial class Auctions : ContentPage
+	{
+		public Auctions ()
+		{
+			InitializeComponent ();
+		}
+
+		public async void Button_Clicked(object sender, EventArgs e)
+		{
+			//await MobileServiceClient client = new MobileServiceClient ("http://");
+
+		}
+	}
+
+
+}
+
---
+++ xPlatAuction/xPlatAuction/ToDoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace xPlatAuction
+{
+	public class ToDoItem
+	{
+		public string Id {
+			get;
+			set;
+		}
+		public string Text {
+			get;
+			set;
+		}
+		public bool Complete {
+			get;
+			set;
+		}
+	}
+}
+
