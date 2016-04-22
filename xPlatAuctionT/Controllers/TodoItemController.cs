2c0582e26f1d6f241e19acb21dead502ccc6dbeb
Ahmed Jawad <sajawwad@hotmail.com>
2016-04-22 17:08:24 +0800

    Chagnes from Mac

--- xPlatAuctionT/Controllers/TodoItemController.cs
+++ xPlatAuctionT/Controllers/TodoItemController.cs
@@ -36,7 +36,7 @@ namespace xPlatAuctionT.Controllers
         // PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
         public Task<TodoItem> PatchTodoItem(string id, Delta<TodoItem> patch)
         {
-            //Change test for git
+            //Change test for git from Mac
             return UpdateAsync(id, patch);
         }
 
